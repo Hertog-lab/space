dce73889147a4ed6f09e5cf98882951a541ac114
Hertog-lab <80678250+Hertog-lab@users.noreply.github.com>
2022-02-23 14:16:26 +0100

    GridSpawner

--- space/Assets/Scripts/Managers/MapGenerator.cs
+++ space/Assets/Scripts/Managers/MapGenerator.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject[] presets;
-    private void Start()
+    [SerializeField] private int gridX;
+    [SerializeField] private int gridY;
+    [SerializeField] private float offset;
+    [SerializeField] private Vector2 gridOrigin;
+    private float randomRotation;
+    private GameObject[] presets;
+
+    private void Awake()
     {
         presets = Resources.LoadAll<GameObject>("Presets");
-        for (int a = 0; a < presets.Length; a++)
+    }
+
+    private void Start()
+    {
+        for (int x = 0; x < gridX; x++)
         {
-           Instantiate(presets[a],new Vector2(0, 0), Quaternion.identity);
+            for (int y = 0; y < gridY; y++)
+            {
+                randomRotation = Random.Range(0f, 360f);
+                int random = Random.Range(0, presets.Length);
+                Vector2 spawnPosition = new Vector2(x * offset, y * offset) + gridOrigin;
+                GameObject clone = Instantiate(presets[random], spawnPosition, Quaternion.Euler(0f, 0f, randomRotation));
+                if (y == gridY * 0.5 && x == gridX * 0.5)
+                {
+                    Instantiate(Resources.Load<GameObject>("Prefabs/Player"), clone.transform.position, Quaternion.identity);
+                }
+            }
         }
     }
 }
