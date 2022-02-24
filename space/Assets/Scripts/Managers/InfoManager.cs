3c6a6cb710a40d481ab0449a5ab6da2dc30978e9
Hertog-lab <80678250+Hertog-lab@users.noreply.github.com>
2022-02-24 09:38:59 +0100

    some more minor changes

--- space/Assets/Scripts/Managers/InfoManager.cs
+++ space/Assets/Scripts/Managers/InfoManager.cs
@@ -6,13 +6,14 @@ public class InfoManager : MonoBehaviour
 {
     public static InfoManager instance;
 
-    public int _enemyAmount;               //used to show how many enemies are nearby
-    public int _sataliteAmount;           //used to show how many satalites are nearby
-    public GameObject _closestEnemy;     //stores the closest enemy
-    public GameObject _closestSatalite; //stores the closest satalite
-    public bool _scannerIsActive;      //used to check if the scanner is active
-    public int _disconectedSatalites; //used to check how many satalites the player still needs to connect
-    public int _connectedSatalites;  //used to updates the progressbar
+    public int _enemyAmount;                    //used to show how many enemies are nearby
+    public int _sataliteAmount;                //used to show how many satalites are nearby
+    public GameObject _closestEnemy;          //stores the closest enemy
+    public GameObject _closestSatalite;      //stores the closest satalite
+    public bool _scannerIsActive;           //used to check if the scanner is active
+    public int _disconectedSatalites;      //used to check how many satalites the player still needs to connect
+    public int _connectedSatalites;       //used to updates the progressbar
+    public Transform lastPlayerLocation; //gets updated to the last player location
 
     private void Awake()
     {
